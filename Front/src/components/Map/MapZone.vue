a79965d6508dcc89c925609fed777fca3be34352
thomas <thomchevalier49@gmail.com>
2023-11-17 11:26:16 +0100

    S'il vous plait, la fatigue me rends fou, /map de nouveau fonctionnel

--- Front/src/components/Map/MapZone.vue
+++ Front/src/components/Map/MapZone.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="map-container">
-    <div id="map"></div>
+  <div>
+    <div class="map-container">
+      <div id="map"></div>
+    </div>
+    <!-- Utilisez un élément pour afficher la modal -->
+
+    <ModalStand @close="toggleModal" :modalActive="modalActive" :stand="selectedStand">
+      <!-- Contenu personnalisé ici -->
+    </ModalStand>
   </div>
 </template>
+
 
 <script>
 import L from 'leaflet';
 import {mapActions, mapGetters, mapMutations, mapState} from 'vuex';
 
 
+import ModalStand from './ModalStand.vue';
+
+
 export default {
+
   data() {
     return {
       map: null,
+      selectedStand: null,
       polygons: [],
+      modalActive: false
 
     };
   },
@@ -26,6 +40,11 @@ export default {
 
     ...mapState(['areas', 'zones']),
 
+    ...mapGetters(['getAreas', 'getSelectedZone', 'getSelectedType']),
+
+    isAreaSelected() {
+      return this.$store.getters.getIsAreaSelected
+    }
   },
 
   created() {
@@ -35,7 +54,10 @@ export default {
 
   methods: {
 
+
     ...mapActions(['getAreas', 'getZones']),
+
+
 
     initializeMap() {
       this.map = L.map('map').setView([48.857572, 2.2977709], 13);
@@ -47,8 +69,16 @@ export default {
       this.updateMap(); // Appel initial de la mise à jour de la carte
     },
     showZoneInfo(zone) {
-      // Affichez les informations de la zone
-      console.log(zone);
+      console.log(zone); // Vérifiez si les données zone sont correctes
+      this.selectedStand = zone;
+      this.$store.commit('SET_IS_AREA_SELECTED', true);
+      console.log('state: ' + this.$store.getters.getIsAreaSelected);
+      this.modalActive = true;
+
+    },
+    closeModal() {
+      this.selectedStand = null; // Réinitialise selectedStand
+      this.$store.commit('SET_IS_AREA_SELECTED', false);
     },
     updateMap() {
       console.log('update');
@@ -60,18 +90,16 @@ export default {
       const selectedZoneIds = this.getSelectedZone;
       const selectedTypeIds = this.getSelectedType;
 
-
-      console.log('Zone selectionné' + selectedZoneIds);
-      console.log('Type de prestation selectionnée' + selectedTypeIds)
+      console.log("test de mes burnes", this.getAreas)
 
       const areas = this.areas;
 
 
       const filteredAreas = areas.filter(zone => {
         return (
-            (zone.isfree === false) &&
+            (zone.isfree===false)&&
             (selectedZoneIds.length === 0 || selectedZoneIds.includes(zone.id_zone)) &&
-            (selectedTypeIds.length === 0 || selectedTypeIds.some(id => zone.id_type_prestation.includes(id)))
+            (selectedTypeIds.length === 0 || selectedTypeIds.some(id => zone.id_type_zone.includes(id)))
         );
       });
 
@@ -89,12 +117,22 @@ export default {
         this.polygons.push(polygon);
       });
     },
-    ...mapMutations(['SET_SELECTED_ZONE', 'SET_SELECTED_TYPE']),
+    toggleModal() {
+      this.modalActive = !this.modalActive;
+      if (!this.modalActive) {
+        this.selectedStand = null; // Réinitialiser lors de la fermeture de la modal
+      }
+    },
+    ...mapMutations(['SET_SELECTED_ZONE', 'SET_SELECTED_TYPE','SET_IS_AREA_SELECTED']),
   },
   watch: {
     // Surveillez les changements dans les sélections
     getSelectedZone: 'updateMap',
     getSelectedType: 'updateMap',
+  },
+
+  components: {
+    ModalStand // Enregistrez le composant ModalStand
   },
 };
 </script>
---
+++ Front/src/components/Map/ModalStand.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <transition name="modal-animation">
+      <div v-if="modalActive" class="overlay">
+        <transition name="modal-animation-inner">
+          <div class="modal-inner">
+            <button class="close-btn" @click="$emit('close')">×</button>
+            <div class="modal-content">
+              <h1>{{ stand.nom_stand }}</h1>
+              <p>{{ stand.description_stand }}</p>
+            </div>
+            <button type="button" class="btn btn-success">Prestations</button>
+
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['modalActive', 'stand']
+};
+</script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(5px); /* Effet de flou sur l'arrière-plan, pas sur la modal */
+  z-index: 1000; /* Assurez-vous que l'overlay est au-dessus des autres éléments */
+}
+
+.modal-inner {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1001; /* Plus élevé que l'overlay pour éviter le flou */
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  background: transparent;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+</style>
