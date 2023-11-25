e85ddeb265882972d5d166d5430337e013bd967f
thomas <thomchevalier49@gmail.com>
2023-11-25 14:25:16 +0100

    réorganisation du dossier Views

---
+++ Front/src/views/Admin/Zone/AdminZonePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main" style="margin-top: 10vh;  width: 90%;">
+    <show-zone></show-zone>
+  </div>
+
+</template>
+
+
+<script>
+import ShowZone from '@/components/Admin/Zone/ShowZone.vue'
+
+
+export default {
+  components: {
+    ShowZone
+  }
+}
+</script>
+
+<style scoped>
+
+.main{
+  display: flex;
+  justify-content: center;
+}
+
+</style>
