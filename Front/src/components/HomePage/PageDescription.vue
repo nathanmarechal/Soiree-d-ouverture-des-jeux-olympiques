ac1803fc8c4ed6fe83ed8ab06f6be7695f3a0ca5
timon <timondubreuil@outlook.com>
2023-12-05 13:43:19 +0100

    traduction de la page de description

--- Front/src/components/HomePage/PageDescription.vue
+++ Front/src/components/HomePage/PageDescription.vue
@@ -5,51 +5,52 @@
         <img class="img" :src="image" alt="image du parcours">
       </div>
       <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center" style="font-family: Arial, sans-serif">
-        <h2 class="subtitle subMarge">Une expérience inoubliable le long des quais de Paris</h2>
+        <h2 class="subtitle subMarge">{{translate("desc_1")}}</h2>
         <div style="text-align: justify; margin-left: 7%; margin-right: 7%;">
           <ul >
-        <li style="margin-bottom: 5px;"><b>Un Voyage à Travers l'Histoire et la Culture : </b>De l'Hôtel de Ville aux jardins des Tuileries, chaque site choisi offre un aperçu de l'histoire et de la beauté de Paris. Ces lieux ne sont pas seulement des fonds scéniques, mais des participants actifs à la célébration.</li>
-        <li style="margin-bottom: 5px"><b>Des Moments Inoubliables : </b> La parade des athlètes et la présentation de la flamme olympique s'entremêlent avec des spectacles culturels vibrants, offrant une immersion totale dans l'esprit des Jeux.</li>
-        <li style="margin-bottom: 5px"><b>Feu d'Artifice et Concerts :  </b>L'apogée de la soirée se trouve près de la Tour Eiffel avec des concerts et un feu d'artifice spectaculaire. Constituant le véritable départ des Jeux.</li>
-        <li style="margin-bottom: 5px"><b>Navigation Facilitée : </b>Notre carte interactive (trouvable en bas de la page) est conçue pour vous guider à travers ces site, et vous fournit toutes les clés pour passer une formidable soirée.</li>
+        <li style="margin-bottom: 5px;"><b>{{translate("desc_2")}}
+        </b>{{translate("desc_3")}}</li>
+        <li style="margin-bottom: 5px"><b>{{translate("desc_4")}}</b>{{translate("desc_5")}}</li>
+        <li style="margin-bottom: 5px"><b>{{translate("desc_6")}}</b>{{translate("desc_7")}}</li>
+        <li style="margin-bottom: 5px"><b>{{translate("desc_8")}}</b>{{translate("desc_9")}}</li>
           </ul>
-        <p style="margin-bottom: 10%"><b>Ces Jeux Olympiques à Paris ne sont pas seulement un événement sportif, mais également une célébration culturelle.</b></p>
+        <p style="margin-bottom: 10%"><b>{{translate("desc_10")}}</b></p>
 
         </div>
         <div class="table-responsive">
           <table class="table">
           <thead>
           <tr>
-            <th scope="col">horaire</th>
-            <th scope="col">prestation</th>
-            <th scope="col">lieu d'observation</th>
+            <th scope="col">{{ translate("desc_horaire") }}</th>
+            <th scope="col">{{ translate("desc_prestation") }}</th>
+            <th scope="col">{{translate("desc_lieuObservation")}}</th>
           </tr>
           </thead>
           <tbody>
           <tr>
-            <th scope="row">19h</th>
-            <td>Parade des athlètes / présentation de la flamme olympique</td>
-            <td>Quais Notre-Dame / Hôtel de ville</td>
+            <th scope="row">{{translate("desc_19h")}}</th>
+            <td>{{translate("desc_19h_1")}}</td>
+            <td>{{translate("desc_19h_2")}}</td>
           </tr>
           <tr>
-            <th scope="row">20h30</th>
-            <td>Défilé de mode olympique </td>
-            <td>Quais Louvre</td>
+            <th scope="row">{{translate("desc_20h30")}}</th>
+            <td>{{translate("desc_20h30_1")}} </td>
+            <td>{{translate("desc_20h30_2")}}</td>
           </tr>
           <tr>
-            <th scope="row">21h</th>
-            <td >Spectacles culturels internationaux</td>
-            <td>Quais Tuileries</td>
+            <th scope="row">{{translate("desc_21h")}}</th>
+            <td >{{translate("desc_21h_1")}}</td>
+            <td>{{translate("desc_21h_2")}}</td>
           </tr>
           <tr>
-            <th scope="row">22h30</th>
-            <td>Spectacles culturels français</td>
-            <td>Quais grand palais / Invalides</td>
+            <th scope="row">{{translate("desc_22h30")}}</th>
+            <td>{{translate("desc_22h30_1")}}</td>
+            <td>{{translate("desc_22h30_2")}}</td>
           </tr>
           <tr>
-            <th scope="row">00h</th>
-            <td >Concerts et feux d'artifice</td>
-            <td>Quais Tour Eiffel / Trocadéro / Iena</td>
+            <th scope="row">{{ translate("desc_00h") }}</th>
+            <td>{{ translate("desc_00h_1") }}</td>
+            <td>{{ translate("desc_00h_2") }}</td>
           </tr>
           </tbody>
         </table>
@@ -61,8 +62,10 @@
 
 <script>
 import image from '../../assets/HomePage/parcours.jpg';
+import {translate} from "../../lang/translationService.js";
 
 export default {
+  methods: {translate},
   data() {
     return {
       image: image,
