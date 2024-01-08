985e68b07375ef5abebaceb5a06a9ca59f87e11e
thomas <thomchevalier49@gmail.com>
2024-01-08 10:05:59 +0100

    mise a jour graphique register

--- Front/src/components/Register/SelectArea.vue
+++ Front/src/components/Register/SelectArea.vue
@@ -2,9 +2,9 @@
   <div v-if="modalActiveAreaSelection" class="overlay">
     <div class="modal-inner">
       <div class="d-flex justify-content-center">
-          <div class="d-flex flex-row gap-3 justify-content-between" style="height: 50vh;width: 70vh">
-            <FilterMapSelectionArea style="width: 20%"/>
-            <MapSignUp @dataEmplacement="handleDataEmplacement" style="width: 75%; height: 100%"/>
+          <div class="d-flex flex-row gap-3 justify-content-between" style="height: 60vh;width: 100vh">
+            <FilterMapSelectionArea style="width: 25%"/>
+            <MapSignUp @dataEmplacement="handleDataEmplacement" style="width: 70%; height: 100%"/>
           </div>
         </div>
       </div>
