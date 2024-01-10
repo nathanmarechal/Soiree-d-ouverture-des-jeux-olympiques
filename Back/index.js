a63bcb72b42c473ac499244c6dec3fe2470152a5
thomas <thomchevalier49@gmail.com>
2024-01-10 19:36:42 +0100

    changement titre swagger parce que pourquoi pas

--- Back/index.js
+++ Back/index.js
@@ -73,7 +73,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API de la SAE',
+      title: 'API cérémonie ouverture des JO',
       version: '1.0.0',
       description: 'API du site de la cérémonie d\'ouverture des jeux olympiques 2024',
     },
