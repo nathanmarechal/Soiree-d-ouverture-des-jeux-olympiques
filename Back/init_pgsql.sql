4591a6672130282c0f4b57e9283ce71594fe10f1
Natflaz <nathan.marechal@edu.univ-fcomte.fr>
2024-01-10 17:03:41 +0100

    insert

--- Back/init_pgsql.sql
+++ Back/init_pgsql.sql
@@ -821,6 +821,12 @@ INSERT INTO avis_stand_utilisateur(id_stand, id_user, note, commentaire) VALUES
 (5,4,2,'Pas terrible')
 ;
 
+INSERT INTO etat_commande (libelle) VALUES
+('En attente de paiement'),
+('En attente de validation'),
+('Validée'),
+('Annulée');
+
 INSERT INTO text_accueil (description) VALUES ('<p>Les Jeux olympiques d''été de 2024, officiellement appelés les Jeux de la XXXIIIe olympiade de l''ère moderne, sont une compétition multisports internationale devant se dérouler à Paris, en France, du 26 juillet au 11 août 2024. La ville de Los Angeles, aux États-Unis, accueillera les Jeux olympiques d été de 2028.</p>'), ('<p>Le Comité international olympique (CIO) a attribué l''organisation des Jeux olympiques d''été de 2024 à Paris lors de la 131e session du CIO à Lima, au Pérou, le 13 septembre 2017. Paris sera la deuxième ville à accueillir les Jeux olympiques d''été pour la troisième fois, après Londres (1908, 1948 et 2012) et avant Los Angeles (1932, 1984 et 2028).</p>, <p>Les Jeux olympiques d''été de 2024 seront les premiers Jeux olympiques d''été à se dérouler en France depuis les Jeux olympiques d''été de 1924, qui se sont déroulés à Paris. Ils seront également les deuxièmes Jeux olympiques d''été à se dérouler en France après les Jeux olympiques d''été de 1900, qui se sont déroulés à Paris.</p>');
 
 INSERT INTO conversations(id_creator, titre, resolu) VALUES
