e4c10f3f15cf237d3df6556ae053b8128799f069
Mathias <mathias.notter1@orange.fr>
2024-01-10 13:43:56 +0100

    utilisateur service fait

--- Front/src/services/utilisateur.service.js
+++ Front/src/services/utilisateur.service.js
@@ -1,32 +1,27 @@
 import { getRequest, postRequest, deleteRequest, patchRequest } from "@/services/axios.service";
 
 async function getAllUsersFromAPI(session_id) {
-    console.log("sessionid front bg :"+session_id)
     let answer = await getRequest('/users/get?session_id='+session_id, 'GETALLUSERS')
-    //console.log("getAllUsersFromAPI: ", answer)
     return answer
 }
 
 async function getAllUsers(session_id) {
     let answer = await getAllUsersFromAPI(session_id)
-    //console.log("getAllUsers: ", answer)
     return answer
 }
 
-async function getAllUsersAttente(){
-    let answer = await getAllUsersAttenteFromAPI()
-    //console.log("getAllUsers: ", answer)
+async function getAllUsersAttente(session_id){
+    let answer = await getAllUsersAttenteFromAPI(session_id)
     return answer
 }
 
-async function getAllUsersAttenteFromAPI() {
-    let answer = await getRequest('/users/getUserAttente', 'GETALLUSERSATTENTE')
+async function getAllUsersAttenteFromAPI(session_id) {
+    let answer = await getRequest('/users/getUserAttente?session_id='+session_id, 'GETALLUSERSATTENTE')
     return answer
 }
 
 async function getAllStandAttente(){
     let answer = await getAllStandAttenteFromAPI()
-    //console.log("getAllUsers: ", answer)
     return answer
 }
 
@@ -56,60 +51,63 @@ async function refuseUserFromAPI(id,session_id) {
     return answer
 }
 
-async function createUserFromAPI(user) {
-    let answer = await postRequest( '/users/registerClient',user, 'CREATEUSER')
-    console.log("createUserFromAPI: ", answer)
-    return answer
-}
-
-async function createUserWithStand(body) {
-    let answer = await createUserWithStandFromAPI(body)
-    return answer
-}
-
-async function createUserWithStandFromAPI(body) {
-    console.log("createUserWithStandFromAPI: ", JSON.stringify(body))
-    let answer = await postRequest('/users/registerPrestataire', body, 'CREATEUSERWITHSTAND')
-    console.log("createUserFromAPI: ", answer)
-    return answer
-}
-
 async function createUser(user,session_id) {
-
     let answer = await createUserFromAPI(user,session_id)
-//    console.log("createUser: ", answer)
     return answer
 }
 
-async function updateUser(id, body) {
-    console.log("updateUserFrom3: ", body)
-    let answer = await updateUserFromAPI(id, body)
-    //console.log("updateUser: ", answer)
+async function createUserFromAPI(user, session_id) {
+    let answer = await postRequest( '/users/create-user?session_id='+session_id,user, 'CREATEUSER')
     return answer
 }
 
-async function updateUserFromAPI(id, body) {
-    console.log("updateUserFromAPI3: ", body)
-    return patchRequest('/users/update/' + id, body, 'UPDATEUSER')
+async function registerClient(user, session_id) {
+    let answer = await registerClientFromAPI(user, session_id)
+    return answer
+}
+
+async function registerClientFromAPI(user, session_id) {
+    let answer = await postRequest('/users/registerClient?session_id='+session_id, user, 'REGISTERCLIENT')
+    return answer
+}
+
+async function registerPrestataire(session_id, body) {
+    let answer = await RegisterPrestaireFromAPI(session_id, body)
+    return answer
+}
+
+async function RegisterPrestaireFromAPI(session_id, body) {
+    let answer = await postRequest('/users/registerPrestataire?session_id'+session_id, body, 'REGISTERPRESTATAIRE')
+    return answer
+}
+
+async function updateUser(session_id, id, body) {
+    let answer = await updateUserFromAPI(session_id, id, body)
+    return answer
+}
+
+async function updateUserFromAPI(session_id, id, body) {
+    return patchRequest('/users/update?session_id'+session_id, body, 'UPDATEUSER')
 }
 
 async function deleteUser(id,session_id) {
-    console.log("about to deleteUserFromAPI")
     let answer = await deleteUserFromAPI(id,session_id)
-    //console.log("deleteUser: ", answer)
     return answer
 }
 
 async function deleteUserFromAPI(id,session_id) {
-    console.log("deleteUserFromAPI my dudes: ", id,session_id)
     return deleteRequest('/users/delete?id_user='+ id+"&session_id="+session_id, 'DELETEUSER')
 }
 
-async function updateSolde(body) {
-    return patchRequest('/users/updateSolde', body, 'UPDATESOLDE')
+async function updateSolde(session_id, id_user, body) {
+    return await updateSoldeFromAPI(session_id, id_user, body)
 }
 
-async function updateUserCourantWoPassword( session_id,id, body) {
+async function updateSoldeFromAPI(session_id, id_user, body) {
+    return patchRequest('/users/updateSolde?session_id='+session_id+'&id_user='+id_user, body, 'UPDATESOLDE')
+}
+
+async function updateUserCourantWoPassword(session_id, id, body) {
     let answer = await updateUserCourantWoPasswordFromAPI(session_id,id, body)
     return answer
 }
@@ -135,7 +133,8 @@ export {
     getUserFromSessionId,
     updateSolde,
     updateUserCourantWoPassword,
-    createUserWithStand,
+    registerClient,
+    registerPrestataire,
     getAllUsersAttente,
     acceptUser,
     refuseUser,
