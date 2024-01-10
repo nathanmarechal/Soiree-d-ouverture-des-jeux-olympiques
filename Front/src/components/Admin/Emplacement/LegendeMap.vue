f9f582f4ecf4ba7602bda3048b24466fb2df0f59
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-01-10 17:11:43 +0100

    d

--- Front/src/components/Admin/Emplacement/LegendeMap.vue
+++ Front/src/components/Admin/Emplacement/LegendeMap.vue
@@ -3,7 +3,7 @@
     <h2>{{ translate("legendeMap_1") }}</h2>
     <div v-for="(zone, index) in getAllZone" :key="'zone-' + index" class="d-flex align-items-center">
       <div class="cercle" :style="{ background: zone.couleur_hexa }"></div>
-      <span>{{ zone.libelle }} ({{ zone.type_zone_libelle }})</span>
+      <span>{{ zone.libelle }} ({{ getTypeZoneLibelle(zone.id_type_zone) }})</span>
     </div>
     <h2>{{ translate("legendeMap_2") }}</h2>
     <div v-for="(type, index) in getAllTypeEmplacementLogistique" :key="'type-' + index" class="d-flex align-items-center">
@@ -30,12 +30,17 @@ export default {
     //this.zones = await this.getZones();
   },
   computed: {
-    ...mapGetters(['getAllZone','getAllTypeEmplacementLogistique']),
+    ...mapGetters(['getAllZone','getAllTypeEmplacementLogistique', 'getAllTypeZone']),
   },
   methods: {
     translate,
-    ...mapActions(['getZonesStore','getTypeEmplacementLogistiqueStore']),
-
+    ...mapActions(['getZonesStore','getTypeEmplacementLogistiqueStore', 'getTypeZonesStore']),
+    getTypeZoneLibelle(id_type_zone) {
+      const typeZone = this.getAllTypeZone.find(type => type.id_type_zone === id_type_zone);
+      if (typeZone)
+        return typeZone.libelle;
+      return '';
+    },
     getImagePath(imageName) {
       return require(`@/assets/Logos/${imageName}`);
     },
